4a97a3d1863cf7836163808e9793f578ff6d6a19
affandhankwala <affandhankwala@gmail.com>
2020-03-05 21:05:26 -0800

    Final completed work

--- src/MyMiniSearchEngine.java
+++ src/MyMiniSearchEngine.java
@@ -1,12 +1,13 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class MyMiniSearchEngine {
     // default solution. OK to change.
     // do not change the signature of index()
-    private Map<String, List<List<Integer>>> indexes;
-
+    private Map<String, List<List<Integer>>> indexes = new HashMap<>(); //Need to initialize this
+    // private Map<String, List<Integer>> indexes = new HashMap<>();
     // disable default constructor
     private MyMiniSearchEngine() {
     }
@@ -17,15 +18,101 @@ public class MyMiniSearchEngine {
 
     // each item in the List is considered a document.
     // assume documents only contain alphabetical words separated by white spaces.
-    private void index(List<String> texts) {
-        //homework
+    private void index(List<String> texts) {    //This list is bunch of sentences that is being passed in and that sentence has all words we need.
+
+        for(int i = 0; i < texts.size(); i++){
+            String[] words = texts.get(i).split(" "); //this splits each sentence into array of words.
+            for(int j = 0; j < words.length; j++){
+               words[j] = strip(words[j]);
+                if(indexes.containsKey(words[j])){ //If the word is already a key then we wish to add the document as part of the linkedlist.
+                    List<List<Integer>> value = indexes.get(words[j]);
+                    value.get(i).add(j);
+
+                }
+                else{
+                    List<List<Integer>> temp = new ArrayList<List<Integer>>();
+                    for(int l = 0; l < texts.size(); l++){
+                      temp.add(new ArrayList<>());
+                    }
+                    temp.get(i).add(j);
+                    indexes.put(words[j], temp);
+
+                }
+            }
+        }
+
     }
 
     // search(key) return all the document ids where the given key phrase appears.
     // key phrase can have one or two words in English alphabetic characters.
     // return an empty list if search() finds no match in all documents.
     public List<Integer> search(String keyPhrase) {
-        // homework
-        return new ArrayList<>(); // place holder
+        //safety check
+        if(keyPhrase.isEmpty() || indexes.isEmpty())
+            return new ArrayList<>();
+
+        String[] words= keyPhrase.split(" ");
+        words[0] = strip(words[0]);
+        if(!indexes.containsKey(words[0]))
+            return new ArrayList<>();
+        List<List<Integer>> result = clone(indexes.get(words[0]));
+        for(int i = 1; i < words.length; i++) {
+            words[i] = strip(words[i]);
+            if(!indexes.containsKey(words[i]))
+                return new ArrayList<>();
+            List<List<Integer>> temp = clone(indexes.get(words[i]));   //This is the values of all the next word.
+            for (int j = 0; j < result.size(); j++) {
+                if (result.get(j).isEmpty()) {
+                    temp.get(j).clear();
+                } else {
+                    for (int k = 0; k < temp.get(j).size(); k++) {
+                        int l = 0;
+                        while (result.get(j).get(l) < temp.get(j).get(k)) {
+                            l++;
+                            if(l>=result.get(j).size())
+                                break;
+                        }
+                        if(l > 0) --l;
+                        if (result.get(j).get(l) != (temp.get(j).get(k) - 1)) {
+                            temp.get(j).remove(k);
+                            k--;
+                        }
+                    }
+                }
+            }
+            result = temp;
+        }
+        List<Integer> answer = new ArrayList<>();
+
+        for(int i = 0; i < result.size(); i++){
+            if(!result.get(i).isEmpty()){
+                answer.add(i);
+            }
+        }
+
+       return answer;
+
     }
+    public static String strip (String string){
+        string = string.toLowerCase();
+        string = string.replaceAll("[^a-z]", "");
+        return string;
+    }
+    public static void printResult(List<List<Integer>> result){         //Used printResult for testing purposes
+        for(List<Integer> document: result){
+            System.out.print(" - ");
+            for(Integer position : document){
+                System.out.print (position+" ");
+            }
+            System.out.println();
+        }
+    }
+    public static List<List<Integer>> clone(List<List<Integer>> source) {           //This is a deep copy of the list.
+        List<List<Integer>> target = new ArrayList<List<Integer>>();
+        for (int i = 0; i < source.size(); ++i)
+            target.add(new ArrayList<Integer>(source.get(i)));
+
+        return target;
+    }
+
 }
--- src/MyMiniSearchEngineTest.java
+++ src/MyMiniSearchEngineTest.java
@@ -44,7 +44,7 @@ public class MyMiniSearchEngineTest {
 
         String[] inputs = {
                 "rains hello abc",
-                "rains Hello abc",
+                "rai23ns Hello abc",
         };
 
         for (String input : inputs) {
@@ -56,13 +56,94 @@ public class MyMiniSearchEngineTest {
 
     @Test
     public void testFourWord() {
-        // homework
-        assertTrue(false); // place holder
+
+        MyMiniSearchEngine engine = new MyMiniSearchEngine(documents());
+
+        String[] inputs = {
+                "sunday hello world fun",
+                "sun3day he23llo wor523ld 12fun",
+        };
+
+        for (String input : inputs) {
+            List<Integer> result = engine.search(input);
+            assertEquals(1, result.size());
+            assertEquals(List.of(5), result);
+        }
     }
+
 
     @Test
     public void testWordNotFound() {
-        // homework
-        assertTrue(false); // place holder
+        MyMiniSearchEngine engine = new MyMiniSearchEngine(documents());
+        List<Integer> result = engine.search("amazing");
+        assertEquals(0, result.size());
+        assertEquals(List.of(), result);
+
+    }
+
+    @Test
+    public void fullTest() {
+        //documents: empty, search: empty
+        MyMiniSearchEngine engine1= new MyMiniSearchEngine(new ArrayList<>());
+        List<Integer> result1 = engine1.search(new String(""));
+        assertEquals(0, result1.size());
+        assertEquals(List.of(), result1);
+
+        //documents: empty, search:  a word
+        String test2[] = {"hello"};
+        List<Integer> result2 = engine1.search(test2[0]);
+        assertEquals(0, result2.size());
+        assertEquals(List.of(), result2);
+
+        //documents : multiple entries with some caps and non alpha chars
+
+        MyMiniSearchEngine engine2 = new MyMiniSearchEngine(new ArrayList<String> (Arrays.asList(
+                "hello this is my world",
+                "My world is big",
+                "hello in 35different ways",
+                "Seattle Has COLD wEAther",
+                "counter",
+                "COUNTER",
+                "c1o2u3n4t5e6r7",
+                "c~o~u~N~t~e~r",
+                "This is a very long sentence that has lot of words but should pass"
+
+        )));
+        //search: empty
+        List<Integer> result3 = engine2.search("");
+        assertEquals(0, result3.size());
+        assertEquals(List.of(), result3);
+
+        //search: one word - lowercase
+        List<Integer> result4 = engine2.search("my");
+        assertEquals(2, result4.size());
+        assertEquals(List.of(0,1), result4);
+
+        //search: one word - mixed case and non alpha
+        List<Integer> result5 = engine2.search("Co23ld");
+        assertEquals(1, result5.size());
+        assertEquals(List.of(3), result5);
+
+        List<Integer> result5_2 = engine2.search("CoUnTeR");
+        assertEquals(4, result5_2.size());
+        assertEquals(List.of(4, 5, 6, 7), result5_2);
+
+        //search: different phrases mixed cases and some non alpha
+        List<Integer> result6 = engine2.search("my world");
+        assertEquals(2, result6.size());
+        assertEquals(List.of(0,1), result6);
+
+        List<Integer> result7 = engine2.search("in differe35nt wAy8s");
+        assertEquals(1, result7.size());
+        assertEquals(List.of(2), result7);
+
+        List<Integer> result8 = engine2.search("Seattle has cold weather");
+        assertEquals(1, result8.size());
+        assertEquals(List.of(3), result8);
+
+        List<Integer> result9 = engine2.search("This is a very long sentence that has lot of words but should pass");
+        assertEquals(1, result9.size());
+        assertEquals(List.of(8), result9);
+
     }
 }
